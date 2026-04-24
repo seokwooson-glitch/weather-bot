05171212aeff5895914ca0a2c2814a9c54ddc3ed
son <seokwooson@gmail.com>
2026-04-24 20:54:31 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,12 +7,21 @@ from weather import get_weather, format_weather
 load_dotenv()
 TOKEN = os.getenv("TELEGRAM_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
+MODE = os.getenv("MODE", "hourly")
 
 async def send_weather():
     bot = Bot(token=TOKEN)
     w = get_weather("bundang")
-    msg = "Weather Alert\n\n" + format_weather(w)
-    await bot.send_message(chat_id=CHAT_ID, text=msg)
+
+    if MODE == "morning":
+        msg = "Good Morning!\n\n" + format_weather(w)
+        await bot.send_message(chat_id=CHAT_ID, text=msg)
+
+    elif MODE == "hourly":
+        pty = w.get("pty", "")
+        if pty:
+            msg = "Rain Alert!\n\n" + format_weather(w)
+            await bot.send_message(chat_id=CHAT_ID, text=msg)
 
 if __name__ == "__main__":
     asyncio.run(send_weather())
